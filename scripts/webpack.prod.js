e62ab28d9c6c6f5617e77cbb2b9ee36f44949c9f
velen <velencoder@outlook.com>
2020-02-12 14:51:52 +0800

    环指定

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,7 +1,13 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  plugins: [new UglifyJSPlugin()]
+  plugins: [
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 });
