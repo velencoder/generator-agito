987496039e50bdbc60f26d48cefe6fcc10c60319
velen <velencoder@outlook.com>
2020-02-12 16:05:25 +0800

    添加copy loader

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 如果预先定义过环境变量，就将其赋值给`ASSET_PATH`变量，否则赋值为根目录
@@ -37,7 +39,11 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
-    })
+    }),
+    new CopyWebpackPlugin([{
+      from:path.resolve(__dirname, '../public')　,//打包的静态资源目录地址
+      to:'' //打包到dist下面的public
+  }]),
   ],
   output: {
     filename: '[name].[chunkhash].js',
