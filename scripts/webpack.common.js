ddf4493f0e1cca1b95d82d11dd4c03147260cb55
velen <velencoder@outlook.com>
2020-02-12 15:45:57 +0800

    公共路径

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 如果预先定义过环境变量，就将其赋值给`ASSET_PATH`变量，否则赋值为根目录
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
+
 
 module.exports = {
   entry: {
@@ -24,6 +30,9 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Caching'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
     })
   ],
   output: {
